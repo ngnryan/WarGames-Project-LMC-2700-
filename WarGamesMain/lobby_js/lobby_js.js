1a79df34f0c98ed91ef6d7d874dd9ee97e3929d6
ngnryan <ryan.ngn@icloud.com>
2026-04-12 17:06:35 -0400

    Update lobby_js.js

--- WarGamesMain/lobby_js/lobby_js.js
+++ WarGamesMain/lobby_js/lobby_js.js
@@ -1,37 +1,86 @@
-
-
+let screen = "challenge one"
+let typedText = "";
+let correctPassword = "open123";
+let message = "";
 
 function setup() {
-  createCanvas(1440, 900)
+  createCanvas(664, 664);
 }
 
-
-//lobby screen with computer 
-  
-  
 function drawDesk() {
-  //surface
+  let s = 1.66;
+
+  line(0*s, 300*s, 30*s, 250*s);
+  line(400*s, 300*s, 370*s, 250*s);
+  line(0*s, 330*s, 400*s, 330*s);
+  line(30*s, 250*s, 370*s, 250*s);
+
+  line(0*s, 310*s, 400*s, 310*s);
+  line(300*s, 330*s, 300*s, 400*s);
+
+  rect(335*s, 350*s, 50*s, 10*s);
+}
+
+
+
+function draw() {
+  background(500);
   
-  line(0, 300, 30, 250) 
-  line(400, 300, 370, 250)
-  line(0, 330, 400, 330)
-  line(30, 250, 370, 250)
+  if (screen == "lobby") {
+    drawDesk();
+  } else if (screen == "challenge one") {
+    background(0)
+    drawPasswordScreen();
+  } else if (screen == "success") {
+    drawSuccessScreen();
+  }
+}
+
+
+function drawPasswordScreen() {
+  background(0);
+  fill(0, 255, 0);
+  textSize(24);
+
+  text("> ENTER PASSWORD:", 30, 80);
   
-  //detail
-  
-  //cabinet
-  line (0, 310, 400, 310)
-  line(300, 330, 300, 400)
-  
-  rect(335, 350, 50, 10)
-  
-  print(windowHeight, windowWidth)
+  text("Exit", 30, 600)
   
 
+  let hiddenText = "";
+  for (let i = 0; i < typedText.length; i++) {
+    hiddenText += typedText[i];
+  }
+
+  text("> " + hiddenText, 30, 130);
+
+  textSize(18);
+  text(message, 30, 190);
 }
- 
-function draw() {
-  background(220)
-  
-  drawDesk()
+
+function drawSuccessScreen() {
+  background(0);
+  fill(0, 255, 0);
+  textSize(24);
+  text("> ACCESS GRANTED", 30, 80);
+}
+
+function keyPressed() {
+  if (screen !== "challenge one") {
+    return;
+  }
+
+  if (keyCode === BACKSPACE) {
+    typedText = typedText.substring(0, typedText.length - 1);
+  } else if (keyCode === ENTER || keyCode === RETURN) {
+    if (typedText === correctPassword) {
+      message = "ACCESS GRANTED";
+      screen = "success";
+    } else {
+      message = "ACCESS DENIED";
+      typedText = "";
+    }
+  } else if (key.length === 1) {
+    typedText += key;
+  }
 }
